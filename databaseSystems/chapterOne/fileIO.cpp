adb8941f341a95d865e800e8ed2b0cd4e4a20c46
Dominic Angelo <dangelo1@ivytech.edu>
2025-10-31 13:36:50 -0400

    Finalized for submission

--- databaseSystems/chapterOne/fileIO.cpp
+++ databaseSystems/chapterOne/fileIO.cpp
@@ -8,7 +8,8 @@
 #include <iostream>
 #include <vector>
 
-struct participant {
+//describes all of the data for our participant
+struct participant { 
     std::string firstName;
     std::string lastName;
     int age;
@@ -18,15 +19,16 @@ struct participant {
     std::string takesStudyMor() {
         if (participantID % 2 == 0) {
             studyMor = true;
-            return "Yes\n";
+            return "Yes";
         } else {
             studyMor = false;
-            return "No\n";
+            return "No";
         }
     }
 };
 
-struct survey {
+//the data necessary for our survey
+struct survey { 
     int surveyID;
     std::string headaches;
     std::string constipation;
@@ -35,10 +37,11 @@ struct survey {
     std::string canStudyMor;
 };
 
-std::vector<participant> participants;
+std::vector<participant> participants; // these vectors are used to display the participant data
 std::vector<survey> surveys;
 
 void menu();
+void loadData();
 void addNewParticipant();
 void collectSurvey();
 void displayParticipants(); 
@@ -46,13 +49,58 @@ void quit();
 
 int main() {
     std::cout << "Welcome to the Westlake Research Hospital StudyMor Study!\n" << std::endl;
+    loadData();
     menu();
 }
 
+// function will load data from the .dat files intot the vectors
+void loadData() {
+    participants.clear();
+    surveys.clear(); // clears the vectors from old data to prevent duplication
+    participant loadParticipant;
+    std::ifstream participantInput("participant.dat");
+    if (participantInput.is_open()) {
+        while (participantInput >> loadParticipant.firstName
+                                >> loadParticipant.lastName
+                                >> loadParticipant.age
+                                >> loadParticipant.gender
+                                >> loadParticipant.participantID) 
+        {
+            loadParticipant.takesStudyMor();
+            participants.push_back(loadParticipant);
+        }
+        participantInput.close();
+    } else {
+        std::cerr << "Error: Could not open participant.dat\n";
+    }
+
+    survey loadSurvey;
+    std::ifstream surveyInput("survey.dat");
+    if (surveyInput.is_open()) {
+        while (surveyInput >> loadSurvey.surveyID
+                           >> loadSurvey.headaches
+                           >> loadSurvey.constipation
+                           >> loadSurvey.difficultySleeping
+                           >> loadSurvey.sideEffects
+                           >> loadSurvey.canStudyMor) 
+        {
+            surveys.push_back(loadSurvey);
+        }
+        surveyInput.close();
+    } else {
+        std::cerr << "Error: Could not open survey.dat\n";
+    }
+}
+
+// function to add a new participant
 void addNewParticipant() {
     participant newParticipant;
-    int newID = 1;
-    newID += participants.size();
+    int newID = 0;
+    if (participants.empty()) {
+        newID = 1; 
+    } else {
+        newID = participants.back().participantID + 1;
+    }
     newParticipant.participantID = newID; 
     std::cout << "\nEnter the participant's first name: ";
     std::cin >> newParticipant.firstName;
@@ -67,17 +115,23 @@ void addNewParticipant() {
     std::cin >> newParticipant.gender;
     std::cout << "\n" << std::endl;
   
-    participants.push_back(newParticipant);
+    participants.push_back(newParticipant); //pushes the participant data into the vector.
 
+    // writes the participant file out to the .dat file
     std::ofstream participantFile("participant.dat", std::ios::app);
-    participantFile << newParticipant.firstName << " " 
-                    << newParticipant.lastName << " "
-                    << newParticipant.age << " "
-                    << newParticipant.gender << " "
-                    << newParticipant.participantID << std::endl;
-    participantFile.close();
+    if (participantFile.is_open()) { // checks to see if file is open safely
+        participantFile << newParticipant.firstName << " " 
+                        << newParticipant.lastName << " "
+                        << newParticipant.age << " "
+                        << newParticipant.gender << " "
+                        << newParticipant.participantID << std::endl;
+        participantFile.close();
+    } else {
+        std::cerr << "Error: Could not open participant.dat\n";
+    }
 }
 
+//function to collect survey data
 void collectSurvey() {
     survey newSurvey;
     int idChoice;
@@ -91,8 +145,11 @@ void collectSurvey() {
         std::cout << std::endl 
                   << "**************************************************\n";
     }
-
+    /* the id is the simplest way to match the survey with the participant, 
+     *when you enter the idchoice you are entering the chosen participant,
+     *the survey is assigned the input id*/
     std::cin >> idChoice;
+    newSurvey.surveyID = idChoice;
 
     std::cout << "Ask the participant the following questions and enter their responses\n";
     
@@ -115,20 +172,24 @@ void collectSurvey() {
 
     std::cout << "\n" << std::endl;
 
-    surveys.push_back(newSurvey);
+    surveys.push_back(newSurvey); // writes survey data to the vector (this is an unnecessary behavior)
 
+    // writes survey data to .dat file
     std::ofstream surveyFile("survey.dat", std::ios::app);
-
-    surveyFile << "Headache response: " << newSurvey.headaches << std::endl
-               << "Constipation response: " << newSurvey.constipation << std::endl
-               << "Difficulty sleeping response: " << newSurvey.difficultySleeping << std::endl
-               << "Side effects response: " << newSurvey.sideEffects << std::endl
-               << "Able to study more repsonse: " << newSurvey.canStudyMor << std::endl;
-    surveyFile.close();
+    if (surveyFile.is_open()) { // checks to make sure file is open safely
+        surveyFile << newSurvey.surveyID << " "
+                   << newSurvey.headaches << " "
+                   << newSurvey.constipation << " "
+                   << newSurvey.difficultySleeping << " "
+                   << newSurvey.sideEffects << " "
+                   << newSurvey.canStudyMor << std::endl;
+        surveyFile.close();
+    } else {
+        std::cerr << "Error: Could not open survey.dat\n";
+    }
 }
 
-void displayParticipants() {
-    int choice;
+void displayParticipants() { // uses the participants vector to output all participant data in a loop
     for (int i = 0; i < participants.size(); i++) {
         std::cout << "ID: " << participants[i].participantID << "\n"
                   << "Name: " << participants[i].firstName << " " << participants[i].lastName << "\n"
@@ -138,21 +199,9 @@ void displayParticipants() {
         std::cout << std::endl 
                   << "**************************************************\n";
     }
-
     std::cout << "\n" << std::endl;
-
-
-    std::cout << "1. Add a New Participant\n"
-              << "2. Collect Survey for Participant\n"
-              << "3. Display Participants\n"
-              << "4. Quit\n"
-              << std::endl
-              << "Please enter a command to continue: ";
-    std::cin >> choice;
-    if (choice == 4) {
-        std::exit(EXIT_SUCCESS);
-    }
 }
+
 void menu() {
     int choice = 0;
     while (choice != 4) {
@@ -163,7 +212,10 @@ void menu() {
                   << std::endl
                   << "Please enter a command to continue: ";
         std::cin >> choice;
-        std::cin.clear();
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
         switch (choice) {
             case 1:
                 addNewParticipant();
@@ -175,7 +227,7 @@ void menu() {
                 displayParticipants();
                 break;
             case 4:
-                std::cout << "Exiting program";
+                std::exit(EXIT_SUCCESS); //exits the program
                 break;
             default:
                 std::cout << "Try again";
